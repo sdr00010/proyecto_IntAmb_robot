d93dee3594954f3064fc17711c3b975013e2cf7e
Sara Dueñas Romero <sdr00010@red.ujaen.es>
2024-04-17 12:24:38 +0200

    modulos y avanzar commit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env pybricks-micropython
+
 # This program requires LEGO EV3 MicroPython v2.0 or higher
 
 # imports
@@ -12,16 +14,25 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 # imports locales
 import os
 import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), './movimiento')))
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), './comunicacion')))
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), './interno')))
+import time
+from movimiento import avanzar as avn
 
 # ---------------------------------------------------------------------------------------------------------------------------------
 
-""" # Crear el objeto del robot.
+# Crear el objeto del robot.
 robot_LEGO = EV3Brick()
+left_motor = Motor(Port.A)  # motor rueda izquierda
+right_motor = Motor(Port.D) # motor rueda derecha
 
+# Prueba: avanzar hacia delante
+avn.avanzar_adelante(robot_LEGO, left_motor, right_motor, 250)
 
+wait(1000)
+
+# Prueba: avanzar hacia atrás
+avn.avanzar_atras(robot_LEGO, left_motor, right_motor, 250)
+
+""" 
 # 1. Make a sound.
 ev3.speaker.beep()
 
@@ -31,4 +42,5 @@ test_motor = Motor(Port.A)
 test_motor.run_target(500, 90)
 
 # 3. Play another beep sound.
-ev3.speaker.beep(frequency=1000, duration=500) """
+ev3.speaker.beep(frequency=1000, duration=500) 
+"""
--- movimiento/avanzar.py
+++ movimiento/avanzar.py
@@ -1,2 +1,30 @@
-def prueba():
-    print("Hello world")
+#!/usr/bin/env pybricks-micropython
+
+
+# import
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+# funciones: avanzar hacia delante
+def avanzar_adelante(robot: EV3Brick, left_motor: Motor, right_motor: Motor, distancia: float):
+    # Inicializar el DriveBase
+    robot_db = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
+    # Avanzar 20cm
+    robot_db.straight(distancia)  # milímetros que avanza
+    # pitar
+    robot.speaker.beep()
+
+
+# funciones: avanzar hacia atrás
+def avanzar_atras(robot: EV3Brick, left_motor: Motor, right_motor: Motor, distancia: float):
+    # Inicializar el DriveBase
+    robot_db = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
+    # Avanzar 20cm hacia atrás
+    robot_db.straight((distancia)*(-1))  # milímetros que avanza
+    # pitar
+    robot.speaker.beep()
+    
