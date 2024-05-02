7be0546a229df9d5b6a9c9f26a93b7d6ec639521
Sara Dueñas Romero <sdr00010@red.ujaen.es>
2024-05-02 11:33:03 +0200

    cambios sara

--- main.py
+++ main.py
@@ -20,29 +20,71 @@ from movimiento import avanzar as avn
 
 # ---------------------------------------------------------------------------------------------------------------------------------
 
-# Crear el objeto del robot.
-robot_LEGO = EV3Brick()
-left_motor = Motor(Port.A)  # motor rueda izquierda
-right_motor = Motor(Port.D) # motor rueda derecha
+# Crear el objetos
+control = utils.Controlador()
+robot = utils.Robot()
 
 # ---------------------------------------------------------------------------------------------------------------------------------
 
-
 # MAIN DE PRUEBAS
+# 1. Generar caminos
+inicial = [31, 26, 21, 16, 11, 12, 13, 18, 23, 28, 33, 34]
+final = [33, 28, 23, 18, 13, 12, 7, 6, 5, 0]
 
-# Prueba: avanzar 1 casillas hacia delante
-avn.adelante_N_casillas(robot_LEGO, left_motor, right_motor, 1)
+# 2. Realizar camino inicial
+""" print("Se quiere ir de la casilla [30] a la [34], el camino es " + str(inicial))
 
-robot_db = DriveBase(left_motor, right_motor, wheel_diameter=utils.CONFIG["parametros"]["wheel_diameter"], axle_track=utils.CONFIG["parametros"]["axle_track"])
-robot_db.turn(90)
+for casilla in inicial:
+    print("Robot en " + str(robot.casilla_actual) + " mirando a " + str(robot.orientacion))
+    # 1. Comprobar la orientacion
+    correcto, deseado = control.comprobar_orientacion(robot.casilla_actual, casilla, robot.orientacion)
+    if (not correcto): 
+        giro = control.corregir_orientacion(robot.orientacion, deseado)
+        print("\tRobot debe orientarse hacia " + str(deseado) + ", girando " + str(giro) + " grados")
+        # Girar robot
+        robot.drive.turn(giro)
+        robot.robot.speaker.beep()
+        robot.orientacion = deseado
+        print("\t-> Robot en " + str(robot.casilla_actual) + " mirando a " + str(robot.orientacion))
+    # else:
+        print("\tRobot orientado correctamente")
+    # 2. Avanzar a la siquiente casilla
+    print("\tRobot avanza 1 casilla, de [" + str(robot.casilla_actual) + "] a [" + str(casilla) + "]")
+    avn.adelante_N_casillas(robot.robot, robot.left_motor, robot.right_motor, 1)
+    robot.casilla_actual = casilla
+robot.robot.speaker.beep()
+print("Robot recoge el paquete en la casilla " + str(robot.casilla_actual))  """
 
-# Prueba: avanzar 2 casillas hacia delante
-avn.adelante_N_casillas(robot_LEGO, left_motor, right_motor, 2)
 
-robot_db = DriveBase(left_motor, right_motor, wheel_diameter=utils.CONFIG["parametros"]["wheel_diameter"], axle_track=utils.CONFIG["parametros"]["axle_track"])
-robot_db.turn(90)
+# ---------------------------------------------------------------------------------------------------------------------------------
 
-avn.adelante_N_casillas(robot_LEGO, left_motor, right_motor, 1)
-
-avn.atras_N_casillas(robot_LEGO, left_motor, right_motor, 1)
-
+pala_motor = Motor(Port.B)
+# avanzar una casilla
+avn.adelante_N_casillas(robot.robot, robot.left_motor, robot.right_motor, 1)
+# levantar pala
+pala_motor.run_target(500, 90)
+# pala_motor.stop()
+robot.robot.speaker.beep()
+# avanzar al paquete
+avn.avanzar_paquete_1(robot.robot, robot.left_motor, robot.right_motor)
+robot.robot.speaker.beep()
+# bajar pala
+pala_motor.run_target(500, 0)
+# pala_motor.stop()
+robot.robot.speaker.beep()
+# avanzar al centro
+avn.avanzar_paquete_2(robot.robot, robot.left_motor, robot.right_motor)
+robot.robot.speaker.beep()
+# media vuelta
+robot.drive.turn(180)
+robot.robot.speaker.beep()
+# avanzar a la otra
+avn.avanzar_paquete_1(robot.robot, robot.left_motor, robot.right_motor)
+robot.robot.speaker.beep()
+# levantar pala
+pala_motor.run_target(500, 90)
+pala_motor.stop()
+robot.robot.speaker.beep()
+# retroceder
+avn.atras_paquete(robot.robot, robot.left_motor, robot.right_motor)
+robot.robot.speaker.beep()
--- movimiento/avanzar.py
+++ movimiento/avanzar.py
@@ -9,30 +9,42 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 # imports locales
-from interno import utils
+from interno.utils import Controlador
+controlador = Controlador()
 
 # funciones: avanzar hacia delante
 def avanzar_adelante(robot: EV3Brick, left_motor: Motor, right_motor: Motor, distancia: float):
     # Inicializar el DriveBase
-    robot_db = DriveBase(left_motor, right_motor, wheel_diameter=utils.CONFIG["parametros"]["wheel_diameter"], axle_track=utils.CONFIG["parametros"]["axle_track"])
+    robot_db = DriveBase(left_motor, right_motor, wheel_diameter=controlador.CONFIG["parametros"]["wheel_diameter"], axle_track=controlador.CONFIG["parametros"]["axle_track"])
     # Avanzar 20cm
     robot_db.straight(distancia)  # milímetros que avanza
     # pitar
-    robot.speaker.beep()
+    # robot.speaker.beep()
 
 # funciones: avanzar hacia atrás
 def avanzar_atras(robot: EV3Brick, left_motor: Motor, right_motor: Motor, distancia: float):
     # Inicializar el DriveBase
-    robot_db = DriveBase(left_motor, right_motor, wheel_diameter=utils.CONFIG["parametros"]["wheel_diameter"], axle_track=utils.CONFIG["parametros"]["axle_track"])
+    robot_db = DriveBase(left_motor, right_motor, wheel_diameter=controlador.CONFIG["parametros"]["wheel_diameter"], axle_track=controlador.CONFIG["parametros"]["axle_track"])
     # Avanzar 20cm hacia atrás
     robot_db.straight((distancia)*(-1))  # milímetros que retrocede
     # pitar
-    robot.speaker.beep()
+    # robot.speaker.beep()
 
 # funciones: avanzar N casillas hacia delante
 def adelante_N_casillas(robot: EV3Brick, left_motor: Motor, right_motor: Motor, n: int):
-    avanzar_adelante(robot, left_motor, right_motor, n*utils.CONFIG["parametros"]["medida_casilla"])
+    avanzar_adelante(robot, left_motor, right_motor, n*(controlador.CONFIG["parametros"]["medida_casilla"]))
 
 # funciones: avanzar N casillas hacia atrás
 def atras_N_casillas(robot: EV3Brick, left_motor: Motor, right_motor: Motor, n: int):
-    avanzar_atras(robot, left_motor, right_motor, n*utils.CONFIG["parametros"]["medida_casilla"])
+    avanzar_atras(robot, left_motor, right_motor, n*(controlador.CONFIG["parametros"]["medida_casilla"]))
+
+# funciones para recoger el paquete
+def avanzar_paquete_1(robot: EV3Brick, left_motor: Motor, right_motor: Motor):
+    avanzar_adelante(robot, left_motor, right_motor, 200) # medida entre el (centro anterior) hasta la (posicion de coger el paquete)
+    
+def avanzar_paquete_2(robot: EV3Brick, left_motor: Motor, right_motor: Motor):
+    avanzar_adelante(robot, left_motor, right_motor, 60) # medida entre (posicion de coger el paquete) y (centro de la casilla siguiente)
+    
+def atras_paquete(robot: EV3Brick, left_motor: Motor, right_motor: Motor):
+    avanzar_atras(robot, left_motor, right_motor, 200) # medida entre el (centro anterior) hasta la (posicion de coger el paquete)
+    
